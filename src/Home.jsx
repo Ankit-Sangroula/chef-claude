717b00219a6631c8173375a5ebac06f0a1c6d834
Ankit.3k <ankitsangroula2006@gmail.com>
2026-09-13 22:24:49 +0545

    Add the claude form

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
 import Header from "./component/Header";
 
-export default function Home(){
-    return(
-        <div>
-            <Header/>
-            <h1>Hello</h1>
-        </div>
-    );
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <form className="flex justify-center items-center mt-30 gap-3">
+        <input
+          type="text"
+          placeholder="e.g Tomato"
+          className="border border-gray-300 rounded-md px-3 text-sm w-[300px] h-[30px]"
+        />
+        <button className="w-[143px] h-[30px] bg-black text-white rounded-md text-sm font-semibold">
+          + Add ingredient
+        </button>
+      </form>
+    </div>
+  );
 }
