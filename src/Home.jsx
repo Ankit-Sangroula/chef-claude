9b270a0e87e35abd27e02ff600ca9e3f4c4c3981
Ankit.3k <ankitsangroula2006@gmail.com>
2026-09-14 21:14:43 +0545

    progress on chef claude form

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,38 @@
 import Header from "./component/Header";
 
 export default function Home() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Get all the data from the submitted form
+    const formData = new FormData(event.currentTarget);
+    // Get the value of the input named "ingredient"
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient)
+    document.write(ingredients);
+  }
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
   return (
     <div>
       <Header />
-      <form className="flex justify-center items-center mt-30 gap-3">
+      <form
+        className="flex justify-center items-center mt-30 gap-3"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="e.g Tomato"
+          name="ingredient"
           className="border border-gray-300 rounded-md px-3 text-sm w-[300px] h-[30px]"
         />
         <button className="w-[143px] h-[30px] bg-black text-white rounded-md text-sm font-semibold">
           + Add ingredient
         </button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </div>
   );
 }
