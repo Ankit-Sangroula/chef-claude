62de59705ad74b268b12981edf5e07be9b2ce4f6
Ankit.3k <ankitsangroula2006@gmail.com>
2026-09-20 21:59:09 +0545

    Refactor ingredient addition and improve layout consistency in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,7 @@ import Header from "./component/Header";
 export default function Home() {
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    setIngredients(prevIngredients => [
-      ...prevIngredients,
-      newIngredient
-    ]);
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
   const [ingredients, setIngredients] = useState([]);
@@ -31,48 +28,41 @@ export default function Home() {
           className="border border-gray-300 rounded-md px-4 text-lg w-[460px] h-[47px]"
         />
 
-        <button
-          className="w-[188px] h-[47px] bg-[#171717] text-white rounded-md text-base font-semibold"
-        >
+        <button className="w-[188px] h-[47px] bg-[#171717] text-white rounded-md text-base font-semibold">
           + Add ingredient
         </button>
       </form>
 
       {ingredients.length > 0 && (
-
-        
         <section className="max-w-[664px] mx-auto mt-7">
-        
-        <h2 className="text-[30px] font-bold mb-4">
-          Ingredients on hand:
-        </h2>
+          <h2 className="text-[30px] font-bold mb-4">Ingredients on hand:</h2>
 
-        <ul
-          className="list-disc pl-8 text-lg space-y-2"
-          aria-live="polite"
-        >
-          {ingredientsListItems}
-        </ul>
+          <ul className="list-disc pl-8 text-lg space-y-2" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          {ingredients.length > 3 && (
 
-        <div className="flex justify-between items-center bg-[#f3f1eb] rounded-lg px-9 py-7 mt-8">
-          
-          <div>
-            <h3 className="text-[21px] font-medium mb-4">
-              Ready for a recipe?
-            </h3>
 
-            <p className="text-[18px] text-gray-500">
-              Generate a recipe from your list of ingredients.
-            </p>
+
+          <div className="flex justify-between items-center bg-[#f3f1eb] rounded-lg px-9 py-7 mt-8">
+            <div>
+              <h3 className="text-[21px] font-medium mb-4">
+                Ready for a recipe?
+              </h3>
+
+              <p className="text-[18px] text-gray-500">
+                Generate a recipe from your list of ingredients.
+              </p>
+            </div>
+
+            <button className="bg-[#d97757] text-white px-6 py-3 rounded-lg font-medium">
+              Get a recipe
+            </button>
           </div>
+          )}
 
-          <button className="bg-[#d97757] text-white px-6 py-3 rounded-lg font-medium">
-            Get a recipe
-          </button>
-
-        </div>
-      </section>
-  )}
+        </section>
+      )}
     </div>
   );
 }
