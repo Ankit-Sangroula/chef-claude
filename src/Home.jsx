d4758adf69fec5030acd7f44cc0d158dce17ed30
Ankit.3k <ankitsangroula2006@gmail.com>
2026-09-25 22:57:07 +0545

    Refactor ingredient display by integrating Ingredients component for improved structure

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./component/Header";
 import Recipe from "./component/Recipe";
+import Ingredients from "./component/Ingredients";
 
 export default function Home() {
   function addIngredient(formData) {
@@ -45,33 +46,7 @@ export default function Home() {
       </form>
 
       {ingredients.length > 0 && (
-        <section className="max-w-[664px] mx-auto mt-7">
-          <h2 className="text-[30px] font-bold mb-4">Ingredients on hand:</h2>
-
-          <ul className="list-disc pl-8 text-lg space-y-2" aria-live="polite">
-            {ingredientsListItems}
-          </ul>
-          {ingredients.length > 3 && (
-            <div className="flex justify-between items-center bg-[#f3f1eb] rounded-lg px-9 py-7 mt-8">
-              <div>
-                <h3 className="text-[21px] font-medium mb-4">
-                  Ready for a recipe?
-                </h3>
-
-                <p className="text-[18px] text-gray-500">
-                  Generate a recipe from your list of ingredients.
-                </p>
-              </div>
-
-              <button
-                onClick={toggleRecipeShown}
-                className="bg-[#d97757] text-white px-6 py-3 rounded-lg font-medium"
-              >
-                Get a recipe
-              </button>
-            </div>
-          )}
-        </section>
+       <Ingredients/>
       )}
       {recipeShown && (<Recipe/>
       )}
